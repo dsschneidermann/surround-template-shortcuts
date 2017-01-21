8a64290e6c65ee95ba3935e3c5fa8e629a35fc34
Dennis Schneidermann <dssnider@gmail.com>
2017-01-21 02:33:24 +0100

    Initial commit

---
+++ LiveTemplateShortcuts/LiveTemplateShortcutsSettings.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Application.Settings;
+using JetBrains.UI;
+
+namespace LiveTemplateShortcuts
+{
+    [SettingsKey(typeof(UserInterfaceSettings), "LiveTemplate Shortcuts settings")]
+    public class LiveTemplateShortcutsSettings
+    {
+        [SettingsEntry(true, "Enabled")]
+        public bool Enabled { get; set; }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Live Template Shortcuts for ReSharper
